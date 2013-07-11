774bf6daf1ee10820e2b240eeda1f132db516294
Nitsan Shai <shai.nitsan@gmail.com>
2013-07-11 02:28:20 -0400

    took out stupid js, fixed images and added js for that

--- static/js/kaylie.js
+++ static/js/kaylie.js
@@ -13,4 +13,18 @@ $(document).ready(function() {
                 $submenu.slideDown();
         }
     });
+
+    $('.tile-span img').mouseover(function() {
+        $this = $(this);
+	var src = $this.attr('src');
+	var new_src = src.replace('bw', 'small');
+	$this.attr('src', new_src);
+    });
+
+    $('.tile-span img').mouseout(function() {
+        $this = $(this);
+        var src = $this.attr('src');
+        var new_src = src.replace('small', 'bw');
+        $this.attr('src', new_src);
+    });
 });
